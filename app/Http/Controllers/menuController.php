6bfec29f16320926aeb5d0fc30f9bf42a498ee74
Redhair <Redhair20@hotmail.com>
2017-07-07 17:03:08 +0300

    Создание и запуск меню. 01

---
+++ app/Http/Controllers/menuController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class menuController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function viewMenu()
+    {
+        //
+        return view('menu');
+
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+   
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/menu', 'menuController@viewMenu')->name('showMenu');
