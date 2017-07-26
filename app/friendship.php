adfab2b22003cfd262049223e114c2318f5c6011
Redhair <Redhair20@hotmail.com>
2017-07-26 13:48:59 +0300

    Удаление лишней таблицы и чистка

--- app/friendship.php
+++ app/friendship.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Friendship extends Model
 {
-    //
+    //список подтвержденных друзей
 }
--- app/media.php
+++ app/media.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Media extends Model
 {
-    //
+    //ссылка на медийный фаил.
 }
